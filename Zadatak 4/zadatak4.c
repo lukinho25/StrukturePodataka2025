55eab14c09b0ea68f31028caf2b89fe82c6efcfa
lukinho25 <lukakuvacic06@gmail.com>
2025-10-28 20:25:08 +0100

    cetvrta vjezba

---
+++ Zadatak 4/zadatak4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/*
+Head
+firstPoly
+secondPoly
+
+Head = {.coeff = 0, .pow = 0, .next = NULL}
+firstPoly = Head;
+secondPoly = Head;
+q = &Head;
+
+td poly * Position;
+td struct poly {
+    int coeff;
+    int pow;
+    Position next;
+    }Poly;
+
+otvoriti dat
+-buffer
+if(sscanf(buffer, "%d %d %n", &coeff, &pow, &n) n != 2) {
+    
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+
+return 0;
+
+}
