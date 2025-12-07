3c0d7925026f051161aefb4125040412f5876fa7
lukinho25 <lukakuvacic06@gmail.com>
2025-12-07 21:31:24 +0100

    sesta vjezba

--- Zadatak 4/zadatak4.c
+++ Zadatak 4/zadatak4.c
@@ -58,3 +58,122 @@ if(p->coeff == 0)
     
 */
 
+typedef struct poly* Pozicija;
+
+typedef struct poly {
+    int coeff;
+    int pow;
+    Pozicija next;
+} Poly;
+
+Pozicija createTerm(int coeff, int pow) {
+    Pozicija newEl = malloc(sizeof(Poly));
+    if (!newEl) return NULL;
+    newEl->coeff = coeff;
+    newEl->pow = pow;
+    newEl->next = NULL;
+    return newEl;
+}
+
+void insertSorted(Pozicija head, int coeff, int pow) {
+    Pozicija prev = head;
+    Pozicija p = head->next;
+
+    while (p != NULL && p->pow > pow) {
+        prev = p;
+        p = p->next;
+    }
+    if (p != NULL && p->pow == pow) {
+        p->coeff += coeff;
+        if (p->coeff == 0) {
+            prev->next = p->next;
+            free(p);
+        }
+        return;
+    }
+    Pozicija newEl = createTerm(coeff, pow);
+    newEl->next = p;
+    prev->next = newEl;
+}
+
+void readPoly(FILE* f, Pozicija head) {
+    char buffer[1024];
+    int coeff, pow, n, pos = 0;
+
+    if (!fgets(buffer, sizeof(buffer), f)) return;
+
+    while (sscanf(buffer + pos, "%d %d %n", &coeff, &pow, &n) == 2) {
+        insertSorted(head, coeff, pow);
+        pos += n;
+    }
+}
+
+void printPoly(Pozicija head) {
+    Pozicija p = head->next;
+    if (!p) {
+        printf("0\n");
+        return;
+    }
+    while (p != NULL) {
+        printf("%dX^%d ", p->coeff, p->pow);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+void addPoly(Pozicija a, Pozicija b, Pozicija result) {
+    Pozicija pa = a->next;
+    Pozicija pb = b->next;
+
+    while (pa != NULL) {
+        insertSorted(result, pa->coeff, pa->pow);
+        pa = pa->next;
+    }
+    while (pb != NULL) {
+        insertSorted(result, pb->coeff, pb->pow);
+        pb = pb->next;
+    }
+}
+
+void mulPoly(Pozicija a, Pozicija b, Pozicija result) {
+    for (Pozicija pa = a->next; pa != NULL; pa = pa->next) {
+        for (Pozicija pb = b->next; pb != NULL; pb = pb->next) {
+            insertSorted(result, pa->coeff * pb->coeff, pa->pow + pb->pow);
+        }
+    }
+}
+
+int main() {
+    FILE* f = fopen("polinomi.txt", "r");
+    if (!f) {
+        printf("Ne mogu otvoriti datoteku!\n");
+        return -1;
+    }
+
+    Poly first = {0, 0, NULL};
+    Poly second = {0, 0, NULL};
+    Poly sum = {0, 0, NULL};
+    Poly product = {0, 0, NULL};
+
+    readPoly(f, &first);
+    readPoly(f, &second);
+
+    fclose(f);
+
+    printf("Prvi polinom: ");
+    printPoly(&first);
+    printf("Drugi polinom: ");
+    printPoly(&second);
+
+    addPoly(&first, &second, &sum);
+    mulPoly(&first, &second, &product);
+
+    printf("\nZbroj:     ");
+    printPoly(&sum);
+    printf("Umnožak:   ");
+    printPoly(&product);
+
+    return 0;
+}
+
+
--- Zadatak 6/zadatak6.c
+++ Zadatak 6/zadatak6.c
@@ -1,57 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct Artikl* PozicijaA;
+typedef struct Racun* PozicijaR;
 
 typedef struct Artikl {
     char naziv[100];
     int kolicina;
     float cijena;
-    struct Artikl *next;
+    PozicijaA next;
 } Artikl;
 
 typedef struct Racun {
     char datum[11];
-    Artikl *artikli;
-    struct Racun *next;
+    PozicijaA artikli;
+    PozicijaR next;
 } Racun;
 
-Racun* ucitajRacune();
-Racun* umetniRacunSortirano(Racun *lista, Racun *novi);
-Artikl* umetniArtiklSortirano(Artikl *lista, Artikl *novi);
-void obradiDatotekuRacuna(Racun *racun, const char *filename);
+PozicijaR ucitajRacune(void);
+PozicijaR umetniRacunSortirano(PozicijaR lista, PozicijaR novi);
+PozicijaA umetniArtiklSortirano(PozicijaA lista, PozicijaA novi);
+void obradiDatotekuRacuna(PozicijaR racun, const char *filename);
 int datumUOpsegu(const char *d, const char *od, const char *do_);
-void upit(Racun *lista);
-void obrisiSve(Racun *lista);
-
-int main() {
+void upit(PozicijaR lista);
+void obrisiSve(PozicijaR lista);
 
 
-    Racun *lista = ucitajRacune();
+int main(void) {
+
+    PozicijaR lista = ucitajRacune();
     if (!lista) {
         printf("Nema učitanih računa!\n");
         return 0;
     }
-
+    
     upit(lista);
 
     obrisiSve(lista);
     return 0;
 }
 
-
-
-Racun* ucitajRacune() {
+PozicijaR ucitajRacune(void) { //funkcija koja učitava račune iz datoteke "racuni.txt"
     FILE *f = fopen("racuni.txt", "r");
     if (!f) return NULL;
 
-    Racun *lista = NULL;
+    PozicijaR lista = NULL;
     char filename[100];
 
     while (fscanf(f, "%s", filename) == 1) {
         FILE *rf = fopen(filename, "r");
         if (!rf) continue;
 
-        Racun *novi = malloc(sizeof(Racun));
+        PozicijaR novi = malloc(sizeof(Racun));
         novi->artikli = NULL;
         novi->next = NULL;
 
@@ -67,40 +69,44 @@ Racun* ucitajRacune() {
     return lista;
 }
 
-Racun* umetniRacunSortirano(Racun *lista, Racun *novi) {
+PozicijaR umetniRacunSortirano(PozicijaR lista, PozicijaR novi) { //funkcija koja umetne racun u sortiranu listu racuna po datumu
     if (!lista || strcmp(novi->datum, lista->datum) < 0) {
         novi->next = lista;
         return novi;
     }
-    Racun *p = lista;
+
+    PozicijaR p = lista;
     while (p->next && strcmp(novi->datum, p->next->datum) > 0)
         p = p->next;
+
     novi->next = p->next;
     p->next = novi;
     return lista;
 }
 
-Artikl* umetniArtiklSortirano(Artikl *lista, Artikl *novi) {
+PozicijaA umetniArtiklSortirano(PozicijaA lista, PozicijaA novi) { //funkcija koja umetne artikl u sortiranu listu artikala po nazivu
     if (!lista || strcmp(novi->naziv, lista->naziv) < 0) {
         novi->next = lista;
         return novi;
     }
-    Artikl *p = lista;
+
+    PozicijaA p = lista;
     while (p->next && strcmp(novi->naziv, p->next->naziv) > 0)
         p = p->next;
+
     novi->next = p->next;
     p->next = novi;
     return lista;
 }
 
-void obradiDatotekuRacuna(Racun *racun, const char *filename) {
+void obradiDatotekuRacuna(PozicijaR racun, const char *filename) { //funkcija koja obraduje datoteku pojedinog računa i popunjava listu artikala
     FILE *f = fopen(filename, "r");
     char temp[200];
 
-    fgets(temp, sizeof(temp), f); // preskoči datum (već učitan)
+    fgets(temp, sizeof(temp), f); // preskoci datum
 
     while (fgets(temp, sizeof(temp), f)) {
-        Artikl *a = malloc(sizeof(Artikl));
+        PozicijaA a = malloc(sizeof(Artikl));
         a->next = NULL;
 
         char naziv[100];
@@ -112,17 +118,17 @@ void obradiDatotekuRacuna(Racun *racun, const char *filename) {
         a->kolicina = kol;
         a->cijena = cij;
 
-        racun->artikli = umetniArtiklSortirano(racun->artikli, a);
+        racun->artikli = umetniArtiklSortirano(racun->artikli, a); // umetni artikl u sortiranu listu artikala
     }
 
     fclose(f);
 }
 
-int datumUOpsegu(const char *d, const char *od, const char *do_) {
+int datumUOpsegu(const char *d, const char *od, const char *do_) { //funkcija koja provjerava je li datum d u opsegu od do do_
     return strcmp(d, od) >= 0 && strcmp(d, do_) <= 0;
 }
 
-void upit(Racun *lista) {
+void upit(PozicijaR lista) { //funkcija koja obrađuje upit korisnika
     char artikl[100], od[11], do_[11];
 
     printf("Unesite naziv artikla: ");
@@ -135,12 +141,12 @@ void upit(Racun *lista) {
     float ukupno = 0;
     int kolicina = 0;
 
-    for (Racun *r = lista; r; r = r->next) {
-        if (!datumUOpsegu(r->datum, od, do_)) continue;
+    for (PozicijaR r = lista; r; r = r->next) { // prolazak kroz sve račune
+        if (!datumUOpsegu(r->datum, od, do_)) continue; // preskoči račune izvan datuma
 
-        for (Artikl *a = r->artikli; a; a = a->next) {
+        for (PozicijaA a = r->artikli; a; a = a->next) { // prolazak kroz artikle na računu
             if (strcmp(a->naziv, artikl) == 0) {
-                ukupno += a->cijena * a->kolicina;
+                ukupno += a->cijena * a->kolicina; 
                 kolicina += a->kolicina;
             }
         }
@@ -152,14 +158,14 @@ void upit(Racun *lista) {
     printf("Ukupno kupljeno: %d kom\n", kolicina);
 }
 
-void obrisiSve(Racun *lista) {
+void obrisiSve(PozicijaR lista) { //funkcija koja briše sve račune i artikle iz memorije
     while (lista) {
-        Racun *r = lista;
+        PozicijaR r = lista;
         lista = lista->next;
 
-        Artikl *a = r->artikli;
+        PozicijaA a = r->artikli;
         while (a) {
-            Artikl *tmp = a;
+            PozicijaA tmp = a;
             a = a->next;
             free(tmp);
         }
